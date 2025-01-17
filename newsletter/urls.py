272dd3f8fbe828311e87452033f62a511f2dcdf6
Jess Brown <revdes5@gmail.com>
2025-01-17 07:47:47 -0700

    Updated paginator to create all of the needed list pages rather than using queries on the path.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,6 +5,18 @@ from . import views
 
 from .models import Newsletter
 
+def get_paginated_newsletter_pages():
+    """
+    Generate the pagination parameters for all pages.
+    This will yield dictionaries with a 'page' key, for every page of newsletters.
+    """
+    total_newsletters = Newsletter.objects.count()
+    paginate_by = 10  # Match the value in your NewsletterList view
+    num_pages = (total_newsletters // paginate_by) + (1 if total_newsletters % paginate_by > 0 else 0)
+    
+    for page in range(1, num_pages + 1):
+        yield {'page': page}
+
 def get_all_newsletters():
     # This function needs to return an iterable of dictionaries.
     # Dictionaries are required as the URL this distill function is used by
@@ -17,5 +29,17 @@ def get_all_newsletters():
 
 urlpatterns = [
     distill_path('', views.NewsletterList.as_view(), name='newsletterList'),
+    # distill_path(
+    #     '', 
+    #     views.NewsletterList.as_view(), 
+    #     name='newsletterList',
+    #     distill_func=lambda: [{'page': 1}]  # Default to the first page
+    # ),
+    distill_path(
+        'page/<int:page>/', 
+        views.NewsletterList.as_view(), 
+        name='newsletterListPaginated',
+        distill_func=get_paginated_newsletter_pages
+    ),
     distill_path('<slug:slug>/', views.newsletterDetail, name='newsletterDetail', distill_func=get_all_newsletters),
 ]
